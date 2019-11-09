e059413160ab96776411593026b37294842f7753
GwonTaekJoon <kyuzu123123@likelion.org>
2019-11-09 18:56:33 +0900

    test

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+
 
 # Create your views here.
+
+
+def signup(request):
+    if request.method == 'POST':
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create.get(username = request.POST['username'])
+                return render(request, 'accoutns/signup.html',{'error' : 'Username has already been taken'})
+            except User.DoesNotExist:
+                user = user.objects.create_user(
+                    request.POST['username'], passoword = request.POST['password1'])
+                auth.login(request.user)
+                return redirect('index')
+
+        else:
+            return render(reqeust, 'accounts/signup.html', {'error':'Password must match'})
+
+    else:
+        return render(request, 'accounts/signup.html')
+    
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password1']
+        user = auth.authenticate(request, username = username, password = password1)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'login.html', {'error':'아이디 혹은 비밀번호가 맞지 않습니다.'})
+    else:
+        return render(request, 'login.html')
+
+
+
+def logout(request):
+
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('index')
+
+    return render(request,'index.html')
+
+    
