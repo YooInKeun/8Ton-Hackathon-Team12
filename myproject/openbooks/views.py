c40123e0ff2836c2a5b7b1093f17a09afe19c5b8
“MarkusOh” <markumichaeloh@gmail.com>
2019-11-09 21:07:40 +0900

    뷰와 openbooks_index.html 약간 수정

--- myproject/openbooks/views.py
+++ myproject/openbooks/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from openbooks.models import BookInstance
+import math
 
 def home(request):
-    return render(request, 'openbooks_index.html')
+    books_unordered = BookInstance.objects.all()
+    context = {}
+    for book in books_unordered:
+        context[book.id] = math.sqrt(math.pow(book.location_longitude, 2) + math.pow(book.location_latitude, 2))
+    context = { 'books_ordered': sorted(context) }
+    return render(request, 'openbooks_index.html', context)
+
