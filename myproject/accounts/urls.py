10c74aaeef171bfc888c059f87c02a2837e0117b
GwonTaekJoon <kyuzu123123@likelion.org>
2019-11-09 19:49:21 +0900

    account complited

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -6,8 +6,9 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    
     path('signup/', views.signup, name="signup"),
     path('login/', views.login, name="login"),
     path('logout/', views.logout, name="logout"),
+    path('', views.index, name="index"),
 ]
